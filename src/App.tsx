68c03e973723cfae45cace4eea3419444a781144
Erick Rezende <erickbarrosrezende@gmail.com>
2025-04-18 16:57:31 -0300

    feat(App.tsx): Importar o AppRoutes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import { AppRoutes } from "./routes/index.tsx";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <AppRoutes />
+    </>
+  );
 }
 
 export default App;
