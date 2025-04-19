02fac359afbd63fac7608a3f524f10add5774f93
Erick Rezende <erickbarrosrezende@gmail.com>
2025-04-19 14:53:38 -0300

    feat(App.tsx): Importar o ToastContainer e aninha-lo no App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import "./App.css";
 import { AppRoutes } from "./routes/index.tsx";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
     <>
       <AppRoutes />
+      <ToastContainer position="top-center" autoClose={3000} />
     </>
   );
 }
