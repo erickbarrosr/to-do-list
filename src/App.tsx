56979e7ebbd6159eefe48f9e1740d53f735eea86
Erick Rezende <erickbarrosrezende@gmail.com>
2025-04-26 14:38:06 -0300

    feat(App.tsx): Configurar o ToastContainer

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 import "./App.css";
+import AppRoutes from "./routes";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <AppRoutes />
+      <ToastContainer position="top-center" autoClose={3000} />
+    </>
+  );
 }
 
 export default App;
